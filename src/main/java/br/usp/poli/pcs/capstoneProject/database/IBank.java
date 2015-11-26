e3a701cf6deb14f731524ea0ebfb56bb3987037d
Wellington Calligaris <wcalligaris@gmail.com>
2015-11-26 14:41:18 -0200

    bank account interface

--- src/main/java/br/usp/poli/pcs/capstoneProject/database/IBank.java
+++ src/main/java/br/usp/poli/pcs/capstoneProject/database/IBank.java
@@ -10,6 +10,4 @@ import org.sql2o.Sql2o;
 public interface IBank {
 	public List<Bank> getBanks(Sql2o sql2o);
 	public Bank createBank(Sql2o sql2o, Map<String, Object> bankInformation);
-	public boolean hasBankAccount(Sql2o sql2o, Map<String, Object> accountDetails);
-	public String tokenFromAccount(Sql2o sql2o, Map<String, Object> accountDetails);
 }
---
+++ src/main/java/br/usp/poli/pcs/capstoneProject/database/IBankAccount.java
@@ -0,0 +1,12 @@
+package br.usp.poli.pcs.capstoneProject.database;
+
+import java.util.Map;
+import org.sql2o.Sql2o;
+import br.usp.poli.pcs.capstoneProject.models.BankAccount;
+
+public interface IBankAccount {
+	public BankAccount createBankAccount(Sql2o sql2o, Map<String, Object> accountInformation);
+	public boolean deleteBankAccount(Sql2o sql2o, int bankAccountId, int bankId);
+	public boolean hasValidInformation(Sql2o sql2o, Map<String, Object> accountDetails);
+	public String tokenFromAccount(Sql2o sql2o, Map<String, Object> accountDetails);
+}
