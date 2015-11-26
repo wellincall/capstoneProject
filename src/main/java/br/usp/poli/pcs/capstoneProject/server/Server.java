461f2643d293c5aec943a7a4dce6f000df25ff25
Wellington Calligaris <wcalligaris@gmail.com>
2015-11-26 12:14:27 -0200

    Freemarker configured

--- src/main/java/br/usp/poli/pcs/capstoneProject/server/Server.java
+++ src/main/java/br/usp/poli/pcs/capstoneProject/server/Server.java
@@ -1,13 +1,31 @@
 package br.usp.poli.pcs.capstoneProject.server;
 
 import static spark.Spark.*;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import spark.ModelAndView;
+
+import java.util.Map;
+import java.util.HashMap;
+
 import br.usp.poli.pcs.capstoneProject.database.CapstoneConnection;
+import freemarker.cache.ClassTemplateLoader;
+import freemarker.template.Configuration;
 
 public class Server {
 	public static void main(String[] args) {
+		FreeMarkerEngine freeMarkerEngine = new FreeMarkerEngine();
+		Configuration freeMarkerConfiguration = new Configuration();
+		freeMarkerConfiguration.setTemplateLoader(new ClassTemplateLoader(Server.class, "/"));
+		freeMarkerEngine.setConfiguration(freeMarkerConfiguration);
 		get("/hello", (request, response) -> {
-			new CapstoneConnection();
 			return "it works";
+		});
+		
+		get("/page", (request, response) -> {
+			Map<String, Object> attributes = new HashMap<String, Object>();
+			attributes.put("title", "First page! Hello dude");
+			return freeMarkerEngine.render(new ModelAndView(attributes, "page.ftl"));			
 		});
 	}
 }
