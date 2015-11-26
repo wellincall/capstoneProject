1fc936d80d35b48a084c77905394ade11afed0b5
Wellington Calligaris <wcalligaris@gmail.com>
2015-11-26 21:20:59 -0200

    field to validate foreing key to bank

--- src/main/java/br/usp/poli/pcs/capstoneProject/fieldValidators/BankForeignKeyValidator.java
+++ src/main/java/br/usp/poli/pcs/capstoneProject/fieldValidators/BankForeignKeyValidator.java
@@ -1,5 +1,14 @@
 package br.usp.poli.pcs.capstoneProject.fieldValidators;
 
-public class BankForeignKeyValidator {
+import java.util.List;
+import br.usp.poli.pcs.capstoneProject.databaseImplementation.BankDAO;
+import br.usp.poli.pcs.capstoneProject.databaseInterface.CapstoneConnection;
 
+public class BankForeignKeyValidator implements IForeignKeyValidator{
+	public boolean validates(int foreignKey) {
+		CapstoneConnection db = new CapstoneConnection();
+		BankDAO dao = new BankDAO();
+		List<Integer> ids = dao.getBankIds(db.getConnection());
+		return ids.contains(foreignKey);
+	}
 }
