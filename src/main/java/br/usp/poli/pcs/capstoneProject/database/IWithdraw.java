cfcf184a1c5ff2a0923933a08bdd28d39303d450
Wellington Calligaris <wcalligaris@gmail.com>
2015-11-26 14:52:34 -0200

    withdraw interface

---
+++ src/main/java/br/usp/poli/pcs/capstoneProject/database/IWithdraw.java
@@ -0,0 +1,12 @@
+package br.usp.poli.pcs.capstoneProject.database;
+
+import java.util.Map;
+import java.util.List;
+import br.usp.poli.pcs.capstoneProject.models.Withdraw;
+import org.sql2o.Sql2o;
+
+public interface IWithdraw {
+	public List<Withdraw> getUserWithdraws(Sql2o sql2o, int userId);
+	public Withdraw createWithdraw(Sql2o sql2o, Map<String, Object> withdrawInformation);
+	public boolean bankReceivedWithdraw(Sql2o sql2, int withdrawId);
+}
