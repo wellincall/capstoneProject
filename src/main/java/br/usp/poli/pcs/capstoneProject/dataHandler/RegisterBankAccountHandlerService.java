f2744d5e8dd81f4060f70ebff7075be37c508e1e
Wellington Calligaris <wcalligaris@gmail.com>
2015-11-26 23:52:35 -0200

    data handler to register new bank account

--- src/main/java/br/usp/poli/pcs/capstoneProject/dataHandler/RegisterBankAccountHandlerService.java
+++ src/main/java/br/usp/poli/pcs/capstoneProject/dataHandler/RegisterBankAccountHandlerService.java
@@ -6,6 +6,7 @@ import spark.Request;
 import br.usp.poli.pcs.capstoneProject.forms.NewBankAccountForm;
 import br.usp.poli.pcs.capstoneProject.forms.Form;
 import br.usp.poli.pcs.capstoneProject.forms.FormField;
+import br.usp.poli.pcs.capstoneProject.helpers.StringToDateService;
 
 public class RegisterBankAccountHandlerService implements IDataHandlerService {
 
@@ -13,7 +14,13 @@ public class RegisterBankAccountHandlerService implements IDataHandlerService {
 		Map<String, Object> bankAccountInfo = new HashMap<String, Object>();
 		Form bankAccountForm = new NewBankAccountForm();
 		for (FormField formField : bankAccountForm.getFormFields()) {
-			bankAccountInfo.put(formField.getFormFieldId(), request.queryParams(formField.getFormFieldId()));
+			if(formField.getFormFieldId().contains("birthday-date")) {
+				bankAccountInfo.put(formField.getFormFieldId(), StringToDateService.call(request.queryParams(formField.getFormFieldId())));
+			} else if (formField.getFormFieldId().contains("id")) {
+				bankAccountInfo.put(formField.getFormFieldId(), Integer.valueOf(request.queryParams(formField.getFormFieldId())));
+			} else {
+				bankAccountInfo.put(formField.getFormFieldId(), request.queryParams(formField.getFormFieldId()));
+			}
 		}
 		return bankAccountInfo;
 	}
