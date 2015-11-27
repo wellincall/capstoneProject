2e56c855c7f80758b90b8fd9f2156a9a43d912fe
Wellington Calligaris <wcalligaris@gmail.com>
2015-11-27 15:15:56 -0200

    login display form handler

---
+++ src/main/java/br/usp/poli/pcs/capstoneProject/handlers/getHandlers/LoginDisplayHandler.java
@@ -0,0 +1,22 @@
+package br.usp.poli.pcs.capstoneProject.handlers.getHandlers;
+
+import spark.Request;
+import spark.Response;
+import java.util.Map;
+import java.util.HashMap;
+
+import br.usp.poli.pcs.capstoneProject.forms.LoginForm;
+
+public class LoginDisplayHandler extends DefaultGetHandler{
+	
+	public LoginDisplayHandler(Request request, Response response) {
+		super(request, response);
+		filePath = "loginPage.ftl";
+	}
+	
+	public Map<String, Object> process() {
+		Map<String, Object> form = new HashMap<String, Object>();
+		form.put("formField", (new LoginForm()).getFormFields());
+		return form;
+	}
+}
