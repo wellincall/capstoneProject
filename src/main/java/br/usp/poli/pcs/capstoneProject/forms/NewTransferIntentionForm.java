1a2291103907f539b58b0ed64bca9aed64680c14
Wellington Calligaris <wcalligaris@gmail.com>
2015-11-29 16:56:36 -0200

    selecting options for selects in html page

--- src/main/java/br/usp/poli/pcs/capstoneProject/forms/NewTransferIntentionForm.java
+++ src/main/java/br/usp/poli/pcs/capstoneProject/forms/NewTransferIntentionForm.java
@@ -4,18 +4,37 @@ import br.usp.poli.pcs.capstoneProject.forms.components.ForeignKeyField;
 import br.usp.poli.pcs.capstoneProject.fieldValidators.UserForeignKeyValidator;
 import br.usp.poli.pcs.capstoneProject.fieldValidators.UserBankAccountForeignKeyValidator;
 import br.usp.poli.pcs.capstoneProject.database.services.GetUserAccountsService;
+import br.usp.poli.pcs.capstoneProject.database.services.GetUsersService;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.List;
+import br.usp.poli.pcs.capstoneProject.models.User;
+import br.usp.poli.pcs.capstoneProject.models.UserBankAccount;
 
 public class NewTransferIntentionForm extends FormWithForeignKey {
 
 	public NewTransferIntentionForm(int userId) {
 		super();
 		formFields.add(new ForeignKeyField("recipient-id", "Recipient", new UserForeignKeyValidator(), generateOptionsForUser()));
+		formFields.add(new ForeignKeyField("sender-account-id", "Remove amount from", new UserBankAccountForeignKeyValidator(), generateOptionsForBankAccount(userId)));
 	}
 	
 	
 	private Map<String, String> generateOptionsForUser() {
-		return null;
+		Map<String, String> options = new HashMap<String, String>();
+		List<User> users = (new GetUsersService()).call();
+		for (User user : users) {
+			options.put(String.valueOf(user.getId()), user.getPhoneNumber() + user.getName());
+		}
+		return options;
+	}
+	
+	private Map<String, String> generateOptionsForBankAccount(int userId) {
+		Map<String, String> options = new HashMap<String, String>();
+		List<UserBankAccount> accounts = (new GetUserAccountsService()).call(userId);
+		for (UserBankAccount account : accounts) {
+			options.put(String.valueOf(account.getId()), account.getAccountToken());
+		}
+		return options;
 	}
 }
