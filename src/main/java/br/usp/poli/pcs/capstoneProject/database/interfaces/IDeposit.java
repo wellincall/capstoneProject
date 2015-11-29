02dcc6aeb612a619c942a9e2eba03608c451ce3f
Wellington Calligaris <wcalligaris@gmail.com>
2015-11-29 17:50:21 -0200

    changed deposit and withdraw interfaces

--- src/main/java/br/usp/poli/pcs/capstoneProject/database/interfaces/IDeposit.java
+++ src/main/java/br/usp/poli/pcs/capstoneProject/database/interfaces/IDeposit.java
@@ -3,10 +3,11 @@ package br.usp.poli.pcs.capstoneProject.database.interfaces;
 import java.util.List;
 import java.util.Map;
 import org.sql2o.Sql2o;
+import org.sql2o.Connection;
 import br.usp.poli.pcs.capstoneProject.models.Deposit;
 
 public interface IDeposit {
 	public List<Deposit> getUserDeposits(Sql2o sql2o, int userId);
-	public Deposit createDeposit(Sql2o sql2o, Map<String, Object> depositInformation);
-	public boolean bankReceivedDeposit(Sql2o sql2o, int depositId);
+	public Deposit createDeposit(Connection connection, Map<String, Object> depositInformation);
+	public boolean bankReceivedDeposit(Connection connection, int depositId);
 }
--- src/main/java/br/usp/poli/pcs/capstoneProject/database/interfaces/IWithdraw.java
+++ src/main/java/br/usp/poli/pcs/capstoneProject/database/interfaces/IWithdraw.java
@@ -4,9 +4,10 @@ import java.util.Map;
 import java.util.List;
 import br.usp.poli.pcs.capstoneProject.models.Withdraw;
 import org.sql2o.Sql2o;
+import org.sql2o.Connection;
 
 public interface IWithdraw {
 	public List<Withdraw> getUserWithdraws(Sql2o sql2o, int userId);
-	public Withdraw createWithdraw(Sql2o sql2o, Map<String, Object> withdrawInformation);
-	public boolean bankReceivedWithdraw(Sql2o sql2, int withdrawId);
+	public Withdraw createWithdraw(Connection connection, Map<String, Object> withdrawInformation);
+	public boolean bankReceivedWithdraw(Connection connection, int withdrawId);
 }
