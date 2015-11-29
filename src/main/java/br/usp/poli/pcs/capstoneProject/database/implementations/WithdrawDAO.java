ade69f32bb91ba3f7ad6b31ac1a210bf0ead320f
Wellington Calligaris <wcalligaris@gmail.com>
2015-11-29 18:12:42 -0200

    creating withdraw

--- src/main/java/br/usp/poli/pcs/capstoneProject/database/implementations/WithdrawDAO.java
+++ src/main/java/br/usp/poli/pcs/capstoneProject/database/implementations/WithdrawDAO.java
@@ -19,8 +19,18 @@ public class WithdrawDAO implements IWithdraw {
 
 	@Override
 	public Withdraw createWithdraw(Connection connection, Map<String, Object> withdrawInformation) {
-		// TODO Auto-generated method stub
-		return null;
+		int withdrawId = connection.createQuery("INSERT INTO withdraws(bankid, accounttoken, transferintentionid, value, status) VALUES "
+				+ "(:bankId, :accountToken, :transferIntentionId, :value, :status)", true)
+					.addParameter("bankId", withdrawInformation.get("bank-id"))
+					.addParameter("accountToken", withdrawInformation.get("account-token"))
+					.addParameter("tranferIntentionId", withdrawInformation.get("transfer-intention-id"))
+					.addParameter("value", withdrawInformation.get("value"))
+					.addParameter("status", Withdraw.CREATED)
+					.executeUpdate().getKey(Integer.class);
+		Withdraw withdraw = connection.createQuery("SELECT * FROM withdraws WHERE id = :id")
+					.addParameter("id", withdrawId)
+					.executeAndFetchFirst(Withdraw.class);
+		return withdraw;
 	}
 
 	@Override
