57e9ead1d0ae982984aaa8fb02517e38634402c1
Wellington Calligaris <wcalligaris@gmail.com>
2015-11-29 18:02:39 -0200

    status in withdraw and deposit

--- src/main/java/br/usp/poli/pcs/capstoneProject/models/Deposit.java
+++ src/main/java/br/usp/poli/pcs/capstoneProject/models/Deposit.java
@@ -1,6 +1,11 @@
 package br.usp.poli.pcs.capstoneProject.models;
 
 public class Deposit {
+	public static final int CREATED = 0;
+	public static final int DECLINED = 1;
+	public static final int ACCEPTED = 2;
+	public static final int VOIDED = 3;
+	public static final int CONSOLIDATED = 4;
 	private int id;
 	private int bankId;
 	private String accountToken;
