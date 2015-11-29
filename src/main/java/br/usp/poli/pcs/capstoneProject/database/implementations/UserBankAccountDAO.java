c405eb03fdf7fc01f7987e71bfa37b5b400353d9
Wellington Calligaris <wcalligaris@gmail.com>
2015-11-28 22:19:01 -0200

    dao creating association between user and bank account

--- src/main/java/br/usp/poli/pcs/capstoneProject/database/implementations/UserBankAccountDAO.java
+++ src/main/java/br/usp/poli/pcs/capstoneProject/database/implementations/UserBankAccountDAO.java
@@ -8,7 +8,10 @@ import org.sql2o.Connection;
 
 import br.usp.poli.pcs.capstoneProject.database.interfaces.IUserBankAccount;
 import br.usp.poli.pcs.capstoneProject.models.UserBankAccount;
-
+import br.usp.poli.pcs.capstoneProject.models.User;
+import br.usp.poli.pcs.capstoneProject.database.services.GetTokenService;
+import br.usp.poli.pcs.capstoneProject.database.services.GetUserByIdService;
+import br.usp.poli.pcs.capstoneProject.database.services.BankAccountValidatorService;
 
 public class UserBankAccountDAO implements IUserBankAccount{
 
@@ -22,8 +25,16 @@ public class UserBankAccountDAO implements IUserBankAccount{
 	public UserBankAccount associateAccount(Sql2o sql2o, Map<String, Object> associationInformation) {
 		UserBankAccount userAccount = null;
 		try(Connection connection = sql2o.beginTransaction()) {
-			
-			
+			if ((new BankAccountValidatorService()).call(associationInformation)) {
+				String token = (new GetTokenService()).call(associationInformation);
+				int associationId = connection.createQuery("INSERT INTO useraccounts(userid, accounttoken) VALUES (:userId, :token)", true)
+								.addParameter("userId" , Integer.valueOf(String.valueOf(associationInformation.get("user-id"))))
+								.addParameter("token" , token)
+								.executeUpdate().getKey(Integer.class);
+				userAccount = connection.createQuery("SELECT * FROM useraccounts WHERE id = :id")
+									.addParameter("id", associationId)
+									.executeAndFetchFirst(UserBankAccount.class);
+			}			
 			connection.commit();
 		}
 		return userAccount;
