9b846310b3dd677e62d2227e1f80d334c3f04e6e
Wellington Calligaris <wcalligaris@gmail.com>
2015-11-29 17:32:26 -0200

    created status from transfer intention

--- src/main/java/br/usp/poli/pcs/capstoneProject/models/TransferIntention.java
+++ src/main/java/br/usp/poli/pcs/capstoneProject/models/TransferIntention.java
@@ -1,6 +1,10 @@
 package br.usp.poli.pcs.capstoneProject.models;
 
 public class TransferIntention {
+	public static final int CREATED = 0;
+	public static final int DECLINED = 1;
+	public static final int ACCEPTED = 2;
+	public static final int VOIDED = 3;
 	private int id;
 	private double value;
 	private int recipientId;
