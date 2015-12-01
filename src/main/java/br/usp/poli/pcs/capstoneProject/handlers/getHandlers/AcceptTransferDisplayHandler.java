d40a74dfbadd140007f7b064708ec147db45e971
Wellington Calligaris <wcalligaris@gmail.com>
2015-12-01 11:03:22 -0200

    handler to display accept transfer page

--- src/main/java/br/usp/poli/pcs/capstoneProject/handlers/getHandlers/AcceptTransferDisplayHandler.java
+++ src/main/java/br/usp/poli/pcs/capstoneProject/handlers/getHandlers/AcceptTransferDisplayHandler.java
@@ -3,6 +3,8 @@ package br.usp.poli.pcs.capstoneProject.handlers.getHandlers;
 import spark.Request;
 import spark.Response;
 import br.usp.poli.pcs.capstoneProject.forms.AcceptTransferForm;
+import br.usp.poli.pcs.capstoneProject.models.TransferIntention;
+import br.usp.poli.pcs.capstoneProject.database.services.GetTransferIntentionByIdService;
 
 import java.util.Map;
 import java.util.HashMap;
@@ -17,7 +19,12 @@ public class AcceptTransferDisplayHandler extends DefaultGetHandler {
 	public Map<String, Object> process() {
 		Map<String, Object> objects = new HashMap<String, Object>();
 		int userId = Integer.valueOf(String.valueOf(request.session().attribute("user-id")));
-		objects.put("formfields", (new AcceptTransferForm(userId)).getFormFields());
+		int transferId = Integer.valueOf(request.queryParams("transfer-id"));
+		TransferIntention transfer = (new GetTransferIntentionByIdService()).call(transferId, userId);
+		if ( transfer != null) {
+			objects.put("formfields", (new AcceptTransferForm(userId)).getFormFields());
+			objects.put("transfer", transfer);
+		}
 		return objects;
 	}
 }
