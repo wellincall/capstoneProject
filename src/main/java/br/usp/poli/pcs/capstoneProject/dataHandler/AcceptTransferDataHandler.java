d7f3c9170407fc5e068a822196d38621d248a14a
Wellington Calligaris <wcalligaris@gmail.com>
2015-12-01 13:59:49 -0200

    skeleton to accept transfer data handler

---
+++ src/main/java/br/usp/poli/pcs/capstoneProject/dataHandler/AcceptTransferDataHandler.java
@@ -0,0 +1,15 @@
+package br.usp.poli.pcs.capstoneProject.dataHandler;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import spark.Request;
+
+
+public class AcceptTransferDataHandler implements IDataHandlerService {
+	public Map<String, Object> call(Request request) {
+		Map<String, Object> transferData = new HashMap<String, Object>();
+		
+		return transferData;
+	}
+}
