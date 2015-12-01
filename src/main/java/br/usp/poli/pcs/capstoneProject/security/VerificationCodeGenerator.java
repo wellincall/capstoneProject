9cdd5fd0da39bba8c7d6ccedf03038f7c667a18f
Wellington Calligaris <wcalligaris@gmail.com>
2015-12-01 16:39:16 -0200

    verification code generator

---
+++ src/main/java/br/usp/poli/pcs/capstoneProject/security/VerificationCodeGenerator.java
@@ -0,0 +1,16 @@
+package br.usp.poli.pcs.capstoneProject.security;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+public class VerificationCodeGenerator {
+	private static String ALLOWED_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+	public String call() {
+		StringBuffer token = new StringBuffer();
+		for(int i = 0; i < 64; i++) {
+			Random numberGenerator = new SecureRandom();
+			token.append(ALLOWED_CHARS.charAt(numberGenerator.nextInt(ALLOWED_CHARS.length())));
+		}
+		return token.toString();
+	}
+}
