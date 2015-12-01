9c5c6071340e0c637855bf601301662b2ce96819
Wellington Calligaris <wcalligaris@gmail.com>
2015-12-01 10:52:24 -0200

    method to fetch transfer by id and recipient id

--- src/main/java/br/usp/poli/pcs/capstoneProject/database/interfaces/ITransferIntention.java
+++ src/main/java/br/usp/poli/pcs/capstoneProject/database/interfaces/ITransferIntention.java
@@ -7,6 +7,7 @@ import br.usp.poli.pcs.capstoneProject.models.TransferIntention;
 
 public interface ITransferIntention {
 	public TransferIntention createTransferIntention(Sql2o sql2o, Map<String, Object> transferDetails);
+	public TransferIntention getTransferIntentionById(Sql2o sql2o, int transferId, int recipientId);
 	public List<TransferIntention> getPendingUserTransferIntentionsAsRecipient(Sql2o sql2o, int userId);
 	public List<TransferIntention> getPendingUserTransferIntentionsAsSender(Sql2o sql2o, int userId);
 	public boolean declineTransferIntention(Sql2o sql2o, int transferIntentionId, int userId);
