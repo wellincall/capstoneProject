d91c0ccc08cdf3e83788a297ab40a4f4a9b7400c
Wellington Calligaris <wcalligaris@gmail.com>
2015-12-12 16:37:32 -0200

    base to handle get user info

---
+++ src/main/java/br/usp/poli/pcs/capstoneProject/handlers/postHandlers/GetUserInfoHandler.java
@@ -0,0 +1,19 @@
+package br.usp.poli.pcs.capstoneProject.handlers.postHandlers;
+
+import spark.Request;
+import spark.Response;
+
+import br.usp.poli.pcs.capstoneProject.models.User;
+import br.usp.poli.pcs.capstoneProject.database.services.GetUserByIdService;
+
+public class GetUserInfoHandler extends DefaultPostHandler { 
+	public GetUserInfoHandler(Request request, Response response) {
+		super(request, response);
+	}
+
+	public String process() {
+		int userId = request.session().attribute("user-id");
+		User user = (new GetUserByIdService()).call(userId);
+		return "";
+	}
+}
