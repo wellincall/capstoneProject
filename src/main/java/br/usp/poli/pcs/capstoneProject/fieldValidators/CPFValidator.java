66440dd30d23bc0fcf564e939fcfc34c91c8c684
Wellington Calligaris <wcalligaris@gmail.com>
2015-12-12 15:59:35 -0200

    removed mask from cpf

--- src/main/java/br/usp/poli/pcs/capstoneProject/fieldValidators/CPFValidator.java
+++ src/main/java/br/usp/poli/pcs/capstoneProject/fieldValidators/CPFValidator.java
@@ -1,7 +1,7 @@
 package br.usp.poli.pcs.capstoneProject.fieldValidators;
 
 public class CPFValidator implements IValidator {
-	static String CPF_REGEX = "^([0-9]{3}.){2}([0-9]{3}-)([0-9]{2})$";
+	static String CPF_REGEX = "^[0-9]{3}{2}[0-9]{3}[0-9]{2}$";
 	public boolean validates(String value) {	
 		return value.matches(CPF_REGEX);
 	}
